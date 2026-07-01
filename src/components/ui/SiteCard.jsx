fd1f87332f944421718a7e5f6d63556e6b845fd7
acastiblanco97 <castiblanco.castaneda.alyson.val@gmail.com>
2026-07-01 15:07:53 -0500

    Se sustraen las horas de abierto dinamicas de la card mientras se estandariza el tema de horarios.

--- src/components/ui/SiteCard.jsx
+++ src/components/ui/SiteCard.jsx
@@ -7,11 +7,10 @@ export default function SiteCard({data}){
   */  
   return(
     <Card className='w-75 p-3'>
-      <Card.Header>Si esta abierto o cerrado (esperar backend)</Card.Header>
+      {/*<Card.Header>Si esta abierto o cerrado (esperar backend)</Card.Header>*/}
       <Card.Body>
         <Card.Title className='fw-bold fs-3'>{data.hospital}</Card.Title>
         <div>
-          <p className='mt-2 mb-2'>Tipo de Banco (pendiente de backend)</p>
           <p className='mt-2 mb-2'><i className="bi bi-geo-alt-fill card-icons"></i> {data.direccion}</p>
           <p className='mt-2 mb-2'><i className="bi bi-clock-fill card-icons"></i> {data.horario}</p>
           <p className='mt-2 mb-2'><i className="bi bi-droplet-fill card-icons"></i> Necesita con urgencia: {data.tipoSangre}</p>
