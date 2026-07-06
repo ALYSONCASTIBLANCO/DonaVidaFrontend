f264256f0439a26c3282d454e8f01c4a89e1727b
acastiblanco97 <castiblanco.castaneda.alyson.val@gmail.com>
2026-07-06 17:16:32 -0500

    update: Se reemplazan las credenciales forzadas por acceso al backend, se hace validacion y permaniencia de token en la sesion. Quedan pendientes la expiracion del token y el manejo de roles.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 //Este archivo contiene el context, es decir, la columna vertebral de credenciales
 //que nos permite validar que acciones puede hacer el usuario registrado y sus privilegios.
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 //El contexto en este caso es un canal de comunicacion que consume toda la plataforma
 //y llega a cualquier componente sin pasar por props.
 const AuthContext = createContext(null);
@@ -11,13 +11,23 @@ export function AuthProvider({children}){
     //Estado real de autenticacion vive aqui
     const [token, setToken] = useState(null);
 
+    //Necesitamos un estado intermedio para que el sistema alcance
+    //a verificar y pueda capturar cuando aun esta en sesion o 
+    //necesita hacer verificaciones adicionales.
+    const [loading, setLoading] = useState(true);
+
     //Cuando se inicia la sesion, guarda el token
     function login(tokenReceived){
+        //Le damos  el token de sesion para que sobreviva en toda.
+        //la app hasta que expire.
+        localStorage.setItem("token", tokenReceived);
         setToken(tokenReceived);
     }
 
     //Cuando cierro sesion:
     function logout(){
+        //Remuevo el token cuando ya no se usa
+        localStorage.removeItem("token");
         setToken(null);
     }
 
@@ -25,10 +35,21 @@ export function AuthProvider({children}){
     const value = {
         token,
         isAuthenticated: token !==null,
+        loading,
         login,
         logout
     }
+    //Usamos un useEffect para que verifique si esta autenticado y con ello, verifique el token y renderice
+    //a donde tiene que renderizar.
+    useEffect(() => {
+    const savedToken = localStorage.getItem("token");
 
+    if (savedToken) {
+        setToken(savedToken);
+    }
+    //La pongo en falso para que el sistema alcance a verificar primero.
+    setLoading(false);
+    }, []);
     return(
         <AuthContext.Provider value={value}>
             {children}
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,46 @@
 import axios from 'axios'
 
 const BASE_URL = 'https://donavida-vzla-production.up.railway.app';
+const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: 5000
+})
+
+//Interceptor de respuestas incorrectas o fuera del 200 (pendiente para entender toda la arquitectura
+// del sistema de tokens.):
+/*
+api.interceptors.response.use(
+    response => response,
+    error => {
+        if(error.response.status == 401){
+
+        }
+        return Promise.reject(error);
+    }
+);*/
+
+//Interceptor para cuando se necesite token de autenticacion (accesos a dashboards.)
+api.interceptors.response.use((config) => {
+    //Capturamos el token (si hay) y lo dejamos en localStorage para que sobreviva al
+    //refresh de la pagina.
+    const token = localStorage.getItem("token");
+
+    //Si existe el token, lo interceptamos y axios siempre lo mandara en los headers.
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
 
 //Los datos que van a la visualizacion publica se exportan aqui:
-export const getCenters = () => axios.get(`${BASE_URL}/centers`)
+export const getCenters = () => api.get("/centers");
+
+//Request para el login de usuarios se encuentra aqui:
+export const loginRequest = async ({ email, password }) =>{
+    const { data } = await api.post("/auth/login",
+        {
+            email,
+            password
+        })
+    return data.access_token;
+}
